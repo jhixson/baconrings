ca0f488cffa0a1db131d0467c180add2e766f362
adam peruta <aperuta@ithaca.edu>
2012-07-13 12:50:50 -0400

    added site guidelines content

--- application/views/pages/site-guidelines.php
+++ application/views/pages/site-guidelines.php
@@ -1,9 +1,42 @@
 <h1>Site Guidelines</h1>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed justo id mi posuere hendrerit. Donec enim ligula, mollis ac eleifend vel, elementum ac diam. Cras luctus urna ac risus aliquam varius. Maecenas nec dui sapien. Praesent adipiscing erat at diam vehicula sed ultricies turpis faucibus. Nam vel libero orci. Aenean dictum leo facilisis libero rhoncus ornare. Nullam faucibus tristique arcu, id semper dui lacinia et. Maecenas malesuada justo sit amet diam imperdiet viverra.</p>
+<p>These are the official posting guidelines ("Site Guidelines") for www.RateMyCampus.com ("Site", "we", "us" or "our") and is a part of, and an Additional Terms under, our <a href="/pages/terms-of-use">Terms of Use Agreement</a>. The Site provides user generated feedback on individual college campus facilities. Campus ratings should only be posted by students who have attended or are currently attending the specific college or university being rated.</p>
 
-<p>Vestibulum eu est a turpis varius vulputate. Pellentesque sollicitudin enim adipiscing arcu ullamcorper viverra. Vivamus tempus scelerisque adipiscing. Quisque posuere ultrices purus, vel eleifend sapien bibendum quis. Maecenas hendrerit varius nulla, nec blandit dui fermentum mattis. Aliquam lacinia orci ut ante imperdiet venenatis. Pellentesque pellentesque erat id mauris rhoncus rutrum. Duis ultrices est sit amet lorem aliquet eget ultrices nunc tincidunt. Quisque congue purus vel lacus volutpat fermentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse potenti.</p>
+<p>Please review the following guidelines before posting a rating and/or comment on the Site:</p>
+
+<p><strong>DO:</strong>
+<ul>
+<li>Be honest and objective in your assessment of a college, or university;</li>
+<li>Limit your comments to what is in the college's or university's control;</li>
+<li>Proof your comments before submitting. Poor spelling will not cause your rating to be removed. However, poor spelling may result in your rating being discredited by those who read it;</li>
+<li>Leave off your Name, Initials, Pseudo Name, or any sort of identifying mark when posting;</li>
+<li>Remember that negative comments that still offer constructive criticism are useful;</li>
+</ul>
+</p>
+
+<p><strong>DO NOT:</strong>
+<ul>
+<li>Use "definitive" language, (e.g., "always","never","etc.");</li>
+<li>Include any piece of information that may identify you or other students;</li>
+<li>Include any piece of identifiable information for a college faculty or staff member;</li>
+<li>State your opinion as fact;</li>
+<li>Post a rating if you have not attended that college or university;</li>
+<li>Rate a facility more than one time per academic year;</li>
+<li>Reference existing comments or comments that have been deleted by our moderators;</li>
+<li>Post hyperlinks;</li>
+<li>Post in a language other than English. Comments must be written in English only.</li>
+<li>Use profanity, name-calling, and/or vulgarity, derogatory remarks about religion, ethnicity or race, physical appearance, mental and/or physical disabilities;</li>
+</ul>
+</p>
+
+<p>The Site reserves the right to remove any comments deemed as inappropriate, libelous, defamatory, indecent, vulgar or obscene, pornographic, sexually explicit or sexually suggestive, racially, culturally, or ethnically offensive, harmful, harassing, intimidating, threatening, hateful, objectionable, discriminatory, or abusive, or which may or may not appear to impersonate anyone else or that otherwise violate the <a href="/pages/terms-of-use">Terms of Use Agreement</a>.</p>
+
+<p>The "Do Not" section of these Site Guidelines will be strictly enforced and the Site reserves all rights to delete or otherwise take appropriate action regarding any violations. If you see a rating that you believe violates these Site Guidelines, please click the red flag icon next to the comment and state the problem. Such comments will be evaluated by the Site's personnel.</p>
+
+<p>Please note, all the reviews on this website are user submitted and should be treated only as opinions. They are not the opinions of RateMyCampus.com or any of its affiliates.</p>
+
+<p>Please do not flag a rating just because you disagree with it.</p>
+
+
 
 <br /><br />
-
-<p></p>
