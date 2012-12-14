dc69fcbd250483e53895808c68f0f8656d9173e3
adam peruta <aperuta@ithaca.edu>
2012-12-13 20:00:18 -0500

    Revert "added an alphabetical link to the header pop up search box"
    This reverts commit cc2abd8fc39ad7a436d0e1c70fbc589a60e401f4.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -49,8 +49,6 @@
 
   					<input type="submit" value="GO" class="bluebutton" />
 
-            <div id="alphalink"><a href="/directory" style="font-size:10pt;">Alphabetical Directory</a></div>
-
   			</form>
 
   		</div>
